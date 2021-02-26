3b286bf7f3d0f8cf2fe1f5d39e4b3add28fa177d
zaneinsane <70311791+zaneinsane@users.noreply.github.com>
2021-02-26 09:52:33 +0200

    Create User.java
    Create User.java
    Created User class , constructor, private fields needed, getters and setters for private fields

---
+++ LayeredArchitecture/BusinessLayer/User.java
@@ -0,0 +1,57 @@
+package LayeredArchitecture.BusinessLayer;
+
+public class User {
+    private int User_ID;
+    private String User_Name;
+    private String User_Password;
+    private String User_Privilege;
+
+    public User(int Uuser_ID, String Uuser_Name, String Uuser_Password, String Uuser_Privilege) {
+        User_ID = Uuser_ID;
+        User_Name = Uuser_Name;
+        User_Password = Uuser_Password;
+        User_Privilege = Uuser_Privilege;
+    }
+
+    @Override
+    public String toString() {
+        return "User [User_ID=" + User_ID + ", User_Name=" + User_Name + ", User_Password=" + User_Password
+                + ", User_Privilege=" + User_Privilege + "]";
+    }
+
+    public int getUser_ID() {
+        return User_ID;
+    }
+
+    public void setUser_ID(int user_ID) {
+        User_ID = user_ID;
+    }
+
+    public String getUser_Name() {
+        return User_Name;
+    }
+
+    public void setUser_Name(String user_Name) {
+        User_Name = user_Name;
+    }
+
+    public String getUser_Password() {
+        return User_Password;
+    }
+
+    public void setUser_Password(String user_Password) {
+        User_Password = user_Password;
+    }
+
+    public String getUser_Privilege() {
+        return User_Privilege;
+    }
+
+    public void setUser_Privilege(String user_Privilege) {
+        User_Privilege = user_Privilege;
+    }
+
+    
+
+    
+}
