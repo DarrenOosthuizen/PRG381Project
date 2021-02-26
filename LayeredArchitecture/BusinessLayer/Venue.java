4ecf8278ed3a371a8b80996f564fdebbc6debc64
zaneinsane <70311791+zaneinsane@users.noreply.github.com>
2021-02-26 09:47:59 +0200

    Update venue

--- LayeredArchitecture/BusinessLayer/Venue.java
+++ LayeredArchitecture/BusinessLayer/Venue.java
@@ -20,9 +20,49 @@ public class Venue {
         return "Venue [Venue_Address=" + Venue_Address + ", Venue_Email=" + Venue_Email + ", Venue_ID=" + Venue_ID
                 + ", Venue_Name=" + Venue_Name + ", Venue_Phone_Number=" + Venue_Phone_Number + "]";
     }
+//methods
+
+public int getVenue_ID() {
+    return Venue_ID;
+}
+
+public void setVenue_ID(int venue_ID) {
+    Venue_ID = venue_ID;
+}
+
+public String getVenue_Name() {
+    return Venue_Name;
+}
+
+public void setVenue_Name(String venue_Name) {
+    Venue_Name = venue_Name;
+}
+
+public String getVenue_Phone_Number() {
+    return Venue_Phone_Number;
+}
+
+public void setVenue_Phone_Number(String venue_Phone_Number) {
+    Venue_Phone_Number = venue_Phone_Number;
+}
+
+public String getVenue_Email() {
+    return Venue_Email;
+}
+
+public void setVenue_Email(String venue_Email) {
+    Venue_Email = venue_Email;
+}
+
+public String getVenue_Address() {
+    return Venue_Address;
+}
+
+public void setVenue_Address(String venue_Address) {
+    Venue_Address = venue_Address;
+}
 
     
-
 
 
 }
