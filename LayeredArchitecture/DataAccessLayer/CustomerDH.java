785fbb40c73f9876fcbc7cfb5870b6d4a43f9d01
DarrenOosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-03-04 13:47:24 +0200

    Added DeleteCustomer

--- LayeredArchitecture/DataAccessLayer/CustomerDH.java
+++ LayeredArchitecture/DataAccessLayer/CustomerDH.java
@@ -2,8 +2,6 @@
 package LayeredArchitecture.DataAccessLayer;
 
 import LayeredArchitecture.BusinessLayer.Customer;
-import jdk.incubator.jpackage.main.Main;
-
 import java.util.List;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -20,19 +18,18 @@ public class CustomerDH
     public static void main(String[] args) 
     {
         System.out.println("Testing Input");
-        CreateCustomer("Kyle", "Oosthuizen", "0720899534", "Darren.oosth");
+       
     }
 
-    public static void CreateCustomer(String CCCusName, String CCCusSurname, String CCCusCell, String CCCusEmail) 
-    {
-       String InsertCusQuery = ("INSERT INTO tblCustomer (Customer_Name, Customer_Surname, Customer_Cell, Customer_Email) VALUES ('" + CCCusName + "','" + CCCusSurname + "','" + CCCusCell + "','" + CCCusEmail + "')");
-       ResultSet resultSet = null ;
+    public  void CreateCustomer(String CCCusName, String CCCusSurname, String CCCusCell, String CCCusEmail) {
+        String InsertCusQuery = ("INSERT INTO tblCustomer (Customer_Name, Customer_Surname, Customer_Cell, Customer_Email) VALUES ('"
+                + CCCusName + "','" + CCCusSurname + "','" + CCCusCell + "','" + CCCusEmail + "')");
+        
 
-       try (Connection connection = DriverManager.getConnection(DAL.DBConnection);
-            PreparedStatement prepsInsertProduct = connection.prepareStatement(InsertCusQuery, Statement.RETURN_GENERATED_KEYS);) 
-        {
+        try (Connection connection = DriverManager.getConnection(DAL.DBConnection);
+                PreparedStatement prepsInsertProduct = connection.prepareStatement(InsertCusQuery);) {
             prepsInsertProduct.execute();
-            
+
         }
         // Handle any errors that may have occurred.
         catch (Exception e) {
@@ -41,69 +38,54 @@ public class CustomerDH
 
     }
 
-    public List<Customer> ReadAllCustomers() 
-    {
+    public List<Customer> ReadAllCustomers() {
         List<Customer> CustomerList = new ArrayList<Customer>();
         ResultSet resultSet = null;
-       
-        try (Connection connection = DriverManager.getConnection(DAL.DBConnection);
-        Statement statement = connection.createStatement();) 
-        {
 
-            
+        try (Connection connection = DriverManager.getConnection(DAL.DBConnection);
+                Statement statement = connection.createStatement();) {
+
             String selectSql = "SELECT * from tblCustomer";
             resultSet = statement.executeQuery(selectSql);
 
-            
-            while (resultSet.next()) 
-            {
-                CustomerList.add(new Customer(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5))) ; 
+            while (resultSet.next()) {
+                CustomerList.add(new Customer(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3),
+                        resultSet.getString(4), resultSet.getString(5)));
             }
 
-        
-        }
-        catch (SQLException e) 
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
-        } 
+        }
 
         return CustomerList;
-        
+
     }
 
-
-
-    public Customer ReadCustomer(int Customer_ID) 
-    {
+    public Customer ReadCustomer(int Customer_ID) {
         Customer customer = new Customer();
         // Code to read specific record from table and load into object Customer
 
         ResultSet resultSet = null;
-    
+
         try (Connection connection = DriverManager.getConnection(DAL.DBConnection);
-        Statement statement = connection.createStatement();) 
-        {
+                Statement statement = connection.createStatement();) {
 
             // Create and execute a SELECT SQL statement.
             String selectSql = ("SELECT * from tblCustomer WHERE Customer_ID = '" + Customer_ID + "'");
             resultSet = statement.executeQuery(selectSql);
 
             // Print results from select statement
-            while (resultSet.next()) 
-            {
-               customer.setCustomer_ID(resultSet.getInt(1));
-               customer.setCustomer_Name(resultSet.getString(2));
-               customer.setCustomer_Surname(resultSet.getString(3));
-               customer.setCustomer_Cell(resultSet.getString(4));
-               customer.setCustomer_Email(resultSet.getString(5));
+            while (resultSet.next()) {
+                customer.setCustomer_ID(resultSet.getInt(1));
+                customer.setCustomer_Name(resultSet.getString(2));
+                customer.setCustomer_Surname(resultSet.getString(3));
+                customer.setCustomer_Cell(resultSet.getString(4));
+                customer.setCustomer_Email(resultSet.getString(5));
             }
 
-
-        }
-        catch (SQLException e) 
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
-        } 
+        }
 
         return customer;
         // return Customer object
@@ -115,9 +97,21 @@ public class CustomerDH
         // later stage
     }
 
-    public void DeleteCustomer(Customer CustomerObj) {
-        // Code to delete specific Customer from table
-        // Would be better to change the method to return true on succesfull delete at a
-        // later stage
+    public  void DeleteCustomer(int CusID) 
+    {
+        String DeleteCusQuery = ("DELETE FROM tblCustomer WHERE Customer_ID = '" + CusID + "'") ;
+
+       try (Connection connection = DriverManager.getConnection(DAL.DBConnection);
+            PreparedStatement prepsDeleteProduct = connection.prepareStatement(DeleteCusQuery);) 
+        {
+            prepsDeleteProduct.execute();
+            
+        }
+        // Handle any errors that may have occurred.
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        
     }
 }
