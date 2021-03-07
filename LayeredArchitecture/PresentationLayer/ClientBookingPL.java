6e241887f18587def25dc3fc5791ce3567a02dff
zaneinsane <70311791+zaneinsane@users.noreply.github.com>
2021-03-07 11:34:25 +0200

    Update ClientBooking

--- LayeredArchitecture/PresentationLayer/ClientBookingPL.java
+++ LayeredArchitecture/PresentationLayer/ClientBookingPL.java
@@ -1,13 +1,43 @@
 package LayeredArchitecture.PresentationLayer;
 
+import java.sql.Date;
+import java.text.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import LayeredArchitecture.BusinessLayer.Customer;
+import LayeredArchitecture.BusinessLayer.Event;
+import LayeredArchitecture.DataAccessLayer.EventDH;
 
 public class ClientBookingPL 
 {
 
-    public void AddBooking(Customer CustDetials)
+    public static void AddBooking()
     {
         
+
+        //CustDetials.getCustomer_ID();
+        //CustDetials.getCustomer_Name();
+       // LocalDate.now();        
+        //String sBooking_Status="Pending";
+        EventDH event = new EventDH();
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.println("                           Please Select an Event type                          ");
+        
+        List<Event> AllEventList = event.ReadAllEvent();
+
+       // for (Event eventItems : AllEventList) {
+      //      System.out.println(eventItems);
+       // }
+        
+       
+
+    }   
+
+
+    public static void main(String[] args) 
+    {
+        AddBooking();
     }
-    
 }
