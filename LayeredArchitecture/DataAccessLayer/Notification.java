cacee42d0aed4bc33778c3f900fab43761fa5592
Kyle Botha <576383@student.belgiumcampus.ac.za>
2021-03-07 23:43:17 +0200

    Added notification server, and notification.java

---
+++ LayeredArchitecture/DataAccessLayer/Notification.java
@@ -0,0 +1,31 @@
+package LayeredArchitecture.DataAccessLayer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Notification {
+
+    public static void SendMail(String to, String subject, String text) throws IOException {
+        URL url = new URL("http://localhost:3001/SendMail");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json; utf-8");
+        con.setRequestProperty("Accept", "application/json");
+        con.setRequestProperty("to", to);
+        con.setRequestProperty("subject", subject);
+        con.setRequestProperty("text", text);
+        con.setDoOutput(true);
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+        }
+    }
+}
